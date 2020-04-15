3b89901624c34e8e25542171bcc07267899d6f48
chuckki <chuckki@projektorientiert.de>
2020-04-15 12:58:35 +0200

    add

---
+++ src/Resources/contao/dca/tl_member_data.php
@@ -0,0 +1,69 @@
+<?php
+
+
+$GLOBALS['TL_DCA']['tl_member_data'] = array
+(
+	// Config
+	'config' => array
+	(
+		'dataContainer'               => 'Table',
+		'enableVersioning'            => true,
+		'ptable'                      => 'tl_member',
+		'sql' => array
+		(
+			'keys' => array
+			(
+				'id' => 'primary',
+				'pid,start,stop,published' => 'index'
+			)
+		)
+	),
+
+
+	// Fields
+	'fields' => array
+	(
+		'id' => array
+		(
+			'sql'                     => "int(10) unsigned NOT NULL auto_increment"
+		),
+		'pid' => array
+		(
+			'foreignKey'              => 'tl_member.username',
+			'sql'                     => "int(10) unsigned NOT NULL default 0",
+			'relation'                => array('type'=>'belongsTo', 'load'=>'lazy')
+		),
+		'tstamp' => array
+		(
+			'sql'                     => "int(10) unsigned NOT NULL default 0"
+		),
+
+
+
+
+		'published' => array
+		(
+			'exclude'                 => true,
+			'filter'                  => true,
+			'flag'                    => 1,
+			'inputType'               => 'checkbox',
+			'eval'                    => array('doNotCopy'=>true),
+			'sql'                     => "char(1) NOT NULL default ''"
+		),
+		'start' => array
+		(
+			'exclude'                 => true,
+			'inputType'               => 'text',
+			'eval'                    => array('rgxp'=>'datim', 'datepicker'=>true, 'tl_class'=>'w50 wizard'),
+			'sql'                     => "varchar(10) NOT NULL default ''"
+		),
+		'stop' => array
+		(
+			'exclude'                 => true,
+			'inputType'               => 'text',
+			'eval'                    => array('rgxp'=>'datim', 'datepicker'=>true, 'tl_class'=>'w50 wizard'),
+			'sql'                     => "varchar(10) NOT NULL default ''"
+		)
+	)
+
+);
